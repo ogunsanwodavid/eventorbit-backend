786f4597e0f5217ad68b16471317c243d1b849df
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-05-24 21:45:33 +0100

    Changed auth from jwt to express-session

--- src/controllers/auth/signInUser.ts
+++ src/controllers/auth/signInUser.ts
@@ -47,6 +47,9 @@ const signInUser = async (
     if (!isMatch)
       return res.status(400).json({ message: "Invalid email or password." });
 
+    //Parse user's object as req for the next function
+    (req as any).user = user;
+
     next();
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong." });
