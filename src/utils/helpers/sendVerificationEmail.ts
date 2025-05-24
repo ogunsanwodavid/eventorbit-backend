1252e5167ce44f3d47234c922a61d1745f19c01c
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-05-24 14:27:32 +0100

    Fixed sendVerificationEmail functionality

--- src/utils/helpers/sendVerificationEmail.ts
+++ src/utils/helpers/sendVerificationEmail.ts
@@ -2,29 +2,19 @@ import nodemailer from "nodemailer";
 
 export const sendVerificationEmail = async (to: string, token: string) => {
   //Google gmail API Env. variabes
-  const googleAPIClientId = process.env.GOOGLE_GMAIL_API_CLIENT_ID;
-  const googleAPIClientSecret = process.env.GOOGLE_GMAIL_API_CLIENT_SECRET;
-  const googleAPIRefreshToken = process.env.GOOGLE_GMAIL_API_REFRESH_TOKEN;
+  const googleAPIEmailUser = process.env.GOOGLE_GMAIL_API_EMAIL_USER;
+  const googleAPIEmailPass = process.env.GOOGLE_GMAIL_API_EMAIL_PASS;
 
   //Throw error if any gmail API Variable is missing
-  if (!googleAPIClientId) throw new Error("Missing google API Client ID");
-  if (!googleAPIClientSecret)
-    throw new Error("Missing google API Client Secret");
-  if (!googleAPIRefreshToken)
-    throw new Error("Missing google API Refresh Token");
+  if (!googleAPIEmailUser) throw new Error("Missing google API Email User");
+  if (!googleAPIEmailPass) throw new Error("Missing google API Email Pass");
 
   //Create nodemailer transport
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      type: "OAuth2",
-      user: "ogunsanwodavid123@gmail.com",
-      clientId: googleAPIClientId,
-      clientSecret: googleAPIClientSecret,
-      refreshToken: googleAPIRefreshToken,
-      accessToken:
-        "ya29.a0AW4Xtxi-bqb7cRWiXfe-IHZQlNrxzoBh0cIeeJsfFXh1OZMV64f4pJ_nNkcNSAdngLuIphYG5tzC3PPsNWhHzT5wyQmS513WCWbexOfe2M8kYAk_HqEtkfLYzxuDM7M87r1NT9wyn8ojNWqz1wTKVlbTM3ZrvM_3KvmLbLi6aCgYKAcMSARISFQHGX2MiGIcxH0m4yhmFfAkTLzOXiw0175",
-      expires: 99999999999999,
+      user: googleAPIEmailUser,
+      pass: googleAPIEmailPass,
     },
   });
 
