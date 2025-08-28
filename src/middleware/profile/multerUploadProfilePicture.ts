04edae9c8b2828ed0e22dc42cefdbb0a3cb64d70
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-08-28 11:31:50 +0100

    Fixed file upload size limit

--- src/middleware/profile/multerUploadProfilePicture.ts
+++ src/middleware/profile/multerUploadProfilePicture.ts
@@ -2,10 +2,10 @@ import multer from "multer";
 
 //Configure Multer middleware
 //::Accept only jpeg, png and webp images
-//::Restrict file size to 5MB
+//::Restrict file size to 2MB
 const multerUploadProfilePicture = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 2 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const validTypes = ["image/jpeg", "image/png", "image/webp"];
     cb(null, validTypes.includes(file.mimetype));
