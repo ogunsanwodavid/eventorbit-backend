e7c4878a7004f484fa903938a11034cfc709367c
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2025-09-09 16:29:38 +0100

    Minor fixes

--- src/utils/schema-validations/events/TimeSlotSchema.ts
+++ src/utils/schema-validations/events/TimeSlotSchema.ts
@@ -2,29 +2,6 @@ import { z } from "zod";
 
 import TimeSchema from "./TimeSchema";
 
-/* 
-const TimeSlotSchema = z
-  .object({
-    startTime: TimeSchema,
-    endTime: TimeSchema,
-  })
-  .superRefine((data, ctx) => {
-    const { startTime, endTime } = data;
-
-    if (
-      startTime.hours > endTime.hours ||
-      (startTime.hours === endTime.hours &&
-        startTime.minutes >= endTime.minutes)
-    ) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: "End time must be after start time",
-        path: ["endTime"],
-      });
-    }
-  }); 
-  */
-
 const TimeSlotSchema = z.object({
   startTime: TimeSchema,
   duration: z.object({
